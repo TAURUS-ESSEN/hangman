f87a0f2e4194985f66c0794caaf31873874a2813
TAURUS-ESSEN <reitarovea@gmail.com>
2025-05-08 14:39:29 +0200

    es wurde Funktion restart() hinzugefügt

--- main.js
+++ main.js
@@ -5,17 +5,34 @@ const hangmanImg = document.getElementById("hangmanImg");
 const suchwortContainer = document.getElementById("suchwort");
 const result = document.getElementById("result");
 
-const suchwort = worte[Math.floor(Math.random()*91100)];
+let suchwort = worte[Math.floor(Math.random()*91100)];
 console.log("generiertes suchwort = " +suchwort )
-const suchwortArray = suchwort.toLowerCase().split("");
+let suchwortArray = suchwort.toLowerCase().split("");
 console.log(suchwortArray)
-const versuchArray = suchwortArray.slice().fill("_");
+let versuchArray = suchwortArray.slice().fill("_");
 console.log(versuchArray)
 console.log("---------------------------------------")
 draw(versuchArray) 
 
 const fehlerImgArray = ['hm0.jpg', 'hm1.jpg','hm2.jpg','hm3.jpg','hm4.jpg','hm5.jpg', 'hm6.jpg', 'hm7.jpg'];
 let fehlerIndex = 0;
+
+function restart() {
+    console.log("RESTART")
+    suchwort = worte[Math.floor(Math.random()*91100)];
+    suchwortArray = suchwort.toLowerCase().split("");
+    versuchArray = suchwortArray.slice().fill("_");
+    fehlerIndex = 0;
+    console.log(`${fehlerImgArray[fehlerIndex]}`)
+    hangmanImg.src = `./img/${fehlerImgArray[fehlerIndex]}`;
+    result.textContent = '';
+    console.log("generiertes suchwort = " +suchwort )
+    console.log(suchwortArray)
+    console.log(versuchArray)
+    draw(versuchArray) ;
+}
+
+
 
 document.addEventListener('keydown', function(event) {
     const taste = event.key.toLowerCase();  
@@ -30,19 +47,26 @@ function draw(versuchArray) {
 }
 
 function suchenBuch(taste) {
+    console.log("hallo from suchenbuch")
     let counter = 0;
     suchwortArray.forEach((buch, index) =>  {
+        console.log("проверяем букву " +buch)
         if (buch === taste) {
             versuchArray[index] = buch;
             counter++
+            console.log("EST")
         }
     })
     if (counter === 0) {
-        fehlerIndex++;
-        if (fehlerIndex >= 7) {
-            result.textContent = "Game Over";
-        }
+        console.log("NET")
+        fehlerIndex++;    
+        console.log("количество ошибок уже" +fehlerIndex);    
         hangmanImg.src = `./img/${fehlerImgArray[fehlerIndex]}`
+        if (fehlerIndex === 7) {
+            result.textContent = "Game Over";
+             restart ();
+        }
+    
     }
     else {
                     draw(versuchArray);
@@ -51,9 +75,12 @@ function suchenBuch(taste) {
 }
 
 function wincontrolle(versuchArray) {
+    console.log("hallo from wincontrolle")
     let controlle = versuchArray.some(num => num === "_");
     console.log(controlle)
     if (controlle === false) {
         result.textContent = "ИГРА ОКОНЧЕНА. ТЫ ВЫИГРАЛ";
+        restart();
+ 
     }
 }
